007d3b59eb226fe18314c5d09eb4d38d9ab3ad45
Taylor Huston <taylor.a.huston@gmail.com>
2019-01-30 19:23:38 -0800

    Finish Chapter 3

--- src/Chap3/AccountTest.java
+++ src/Chap3/AccountTest.java
@@ -2,14 +2,35 @@ package Chap3;
 
 //Account class tester
 
+import java.util.Scanner;
+
 public class AccountTest {
 
     public static void main(String[] args) {
 
-        Account account1 = new Account("Jane Green");
-        Account account2 = new Account("John Blue");
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("John Blue", -7.53);
 
-        System.out.printf("account1 name is: %s%n", account1.getName());
-        System.out.printf("account2 name is: %s%n", account2.getName());
+        System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("%nEnter deposit amount for account1");
+        double depositAmount = input.nextDouble();
+        System.out.printf("%nadding %.2f to account1 balance %n%n", depositAmount);
+        account1.deposit(depositAmount);
+
+        System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+
+        System.out.println("%nEnter deposit amount for account2");
+        depositAmount = input.nextDouble();
+        System.out.printf("%nadding %.2f to account2 balance %n%n", depositAmount);
+        account2.deposit(depositAmount);
+
+        System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+
     }
 }
