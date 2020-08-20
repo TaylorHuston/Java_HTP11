70d5a0323f2e109ac18ba47c6abdabde79eeabd0
Taylor Huston <taylor.a.huston@gmail.com>
2020-08-20 14:51:46 -0400

    Typo

--- src/Chap3/AccountTest.java
+++ src/Chap3/AccountTest.java
@@ -16,7 +16,7 @@ public class AccountTest {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("%nEnter deposit amount for account1");
+        System.out.printf("%nEnter deposit amount for Account 1: ");
         double depositAmount = input.nextDouble();
         System.out.printf("%nadding %.2f to account1 balance %n%n", depositAmount);
         account1.deposit(depositAmount);
@@ -24,7 +24,7 @@ public class AccountTest {
         System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
         System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
 
-        System.out.println("%nEnter deposit amount for account2");
+        System.out.printf("%nEnter deposit amount for Account 2: ");
         depositAmount = input.nextDouble();
         System.out.printf("%nadding %.2f to account2 balance %n%n", depositAmount);
         account2.deposit(depositAmount);
